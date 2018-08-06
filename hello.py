6bc968a360a1e8daa9018c06454b2983a36ee77e
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 13:14:51 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+"""All you have to do is type return "hello edabit.com"; between the curly braces and click the Check button. 
+If everything went according to plan, the button will turn red and say SUBMIT FINAL. Click it and see what happens."""
+
 function hello() {
 	return "hello edabit.com";
 }
