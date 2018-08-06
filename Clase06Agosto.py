7656eff2734384f9575a30be1d193649922a3f28
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 17:46:03 -0500

    Update and rename addition.py to Clase06Agosto.py

---
+++ Clase06Agosto.py
@@ -0,0 +1,4 @@
+#Create a function that takes two numbers as arguments and return their sum.
+def addition(a, b) 
+	return a+b
+
--- addition.py
+++
@@ -1,4 +0,0 @@
-"""Create a function that takes two numbers as arguments and return their sum."""
-def addition(a, b) 
-	return a+b
-
