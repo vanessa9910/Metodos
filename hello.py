1bd68d8b2427a1702057120d24e495f74743979e
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 17:46:27 -0500

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-"""All you have to do is type return "hello edabit.com"; between the curly braces and click the Check button. 
-If everything went according to plan, the button will turn red and say SUBMIT FINAL. Click it and see what happens."""
-
-def hello():
-	return "hello edabit.com"
-	
