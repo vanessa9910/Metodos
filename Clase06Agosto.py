0b2499e419792cdf6429011557287eccbe85b9e0
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 17:47:21 -0500

    Update Clase06Agosto.py

--- Clase06Agosto.py
+++ Clase06Agosto.py
@@ -1,4 +1,11 @@
+#All you have to do is type return "hello edabit.com"; between the curly braces and click the Check button. 
+#If everything went according to plan, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+def hello():
+	return "hello edabit.com"
+
 #Create a function that takes two numbers as arguments and return their sum.
 def addition(a, b) 
 	return a+b
+
+
 
