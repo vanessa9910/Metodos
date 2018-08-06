1f97b979bd2dc6b7bcfe9191453ab6e1e6575ebf
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 17:41:24 -0500

    Update and rename Kata1 to Kata1.py

--- Kata1.py
+++ Kata1.py
@@ -1,3 +1,8 @@
+
+#Create a function that takes a list of numbers. Return the largest number in the list.
+def findLargestNum(nums):
+	return max(nums)
+
 #Create a function that takes a list and a string as arguments and return the index of the string.
 def find_index(lst, str):
 	return lst.index(str)
