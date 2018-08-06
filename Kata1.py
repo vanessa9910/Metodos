e9f058beb9e890009e992acf3e9c0f112b4eb479
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-08-06 17:44:06 -0500

    Update Kata1.py

--- Kata1.py
+++ Kata1.py
@@ -1,7 +1,14 @@
 
 #Create a function that takes a list of numbers. Return the largest number in the list.
 def findLargestNum(nums):
-	return max(nums)
+	return max(nums
+		   
+#Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
+def isEvenOrOdd(num):
+	if num % 2 == 0:
+		return "even"
+	else:
+		return "odd"
 
 #Create a function that takes a list and a string as arguments and return the index of the string.
 def find_index(lst, str):
