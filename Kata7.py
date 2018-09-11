79caef6558584c6ae824ccf3b7e1c42abf7b0b98
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-09-11 08:56:29 -0500

    Update Kata7.py

--- Kata7.py
+++ Kata7.py
@@ -5,3 +5,11 @@ def factorial(num):
 		return 1;
 	else:
 		return num * factorial(num-1)
+
+#Take a list of integers (positive or negative or both) and return the sum of the absolute value of each element.
+
+def get_abs_sum(lst):
+	suma = 0;
+	for i in range (len(lst)):
+		suma+=abs(lst[i]);
+	return suma
