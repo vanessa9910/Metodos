25844aa28c9840fb0c98cec059f61faf7d8c439b
vanessa9910 <42152026+vanessa9910@users.noreply.github.com>
2018-11-05 09:40:23 -0600

    Create Kata13.py

---
+++ Kata13.py
@@ -0,0 +1,161 @@
+import matplotlib.pyplot as plt
+import numpy as np
+ # 1) Un agrónomo ha analizado la relación entre la cantidad de agua aplicada (en m3 ) y el correspondiente rendimiento de cierta cosecha (en toneladas por hectárea) obteniendo los siguientes datos 
+
+#Agua, X 8 16 24 32 
+
+#Rendimiento, Y 4.1 4.5 5.1 6.1 
+
+ 
+
+#Obtener el rendimiento que cabe esperar si la cantidad de agua aplicada es de 12 m3 . 
+
+def createMatriz(m,n,v):
+    C = []
+    for i in range(m):
+        C.append([])
+        for j in range(n):
+            C[i].append(v)
+
+    return C
+
+def getDimensiones(A):
+    return (len(A),len(A[0]))
+
+def sumMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
+    if Am != Bm or An != Bn:
+        print("Las dimensiones son diferentes")
+        return []
+    C = createMatriz(Am,An,0)
+    for i in range(Am):
+        for j in range(An):
+            C[i][j] = A[i][j] + B[i][j]
+    return C
+
+def restaMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
+    if Am != Bm or An != Bn:
+        print("Las dimensiones son diferentes")
+        return []
+    C = createMatriz(Am,An,0)
+    for i in range(Am):
+        for j in range(An):
+            C[i][j] = A[i][j] - B[i][j]
+    return C
+
+def mulMatrices(A,B):
+    Am,An = getDimensiones(A)
+    Bm,Bn = getDimensiones(B)
+    if An != Bm:
+        print("Las matrices no son conformables")
+        return []
+    C = createMatriz(Am,Bn,0)
+    for i in range(Am):
+        for j in range(Bn):
+            for k in range(An):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+def getMenorMatriz(A,r,c):
+    m,n = getDimensiones(A)
+    C = createMatriz(m-1,n-1,0)
+    for i in range(m):
+        if i == r:
+            continue
+        for j in range(n):
+            if j == c:
+                continue
+            Ci = i
+            if i > r:
+                Ci = i - 1
+            Cj = j
+            if j > c:
+                Cj = j -1
+            C[Ci][Cj] = A[i][j]
+    return C
+
+def detMatriz(A):
+    m,n = getDimensiones(A)
+    if m != n:
+        print("La matriz no es cuadrada")
+        return -1
+    if m == 1:
+        return m
+    if m == 2:
+        return  A[0][0]*A[1][1] - A[0][1]*A[1][0]
+    det = 0
+    for j in range(n):
+        det += (-1)**(j)*A[0][j]*detMatriz(getMenorMatriz(A,0,j))
+    return det
+
+def getMatrizAdyacente(A):
+    m,n = getDimensiones(A)
+    C = createMatriz(m,n,0)
+    for i in range(m):
+        for j in range(n):
+            C[i][j] = (-1)**(i+j)*detMatriz(getMenorMatriz(A,i,j))
+    return C
+
+def getMatrizTranspuesta(A):
+    m,n = getDimensiones(A)
+    C = createMatriz(n,m,0)
+    for i in range(m):
+        for j in range(n):
+            C[j][i] = A[i][j]
+    return C
+
+def getMatrizInversa(A):
+    detA = detMatriz(A)
+    if detA == 0:
+        print("La matriz no tiene inversa")
+        return 0
+    At =  getMatrizTranspuesta(A)
+    adyAt =  getMatrizAdyacente(At)
+    m,n = getDimensiones(A)
+    C = createMatriz(m,n,0)
+    for i in range(m):
+        for j in range(n):
+            C[i][j] = (1/detA)*adyAt[i][j]
+    return C
+
+x = [8,16,24,32]
+y = [4.1,4.5,5.1,6.1]
+
+plt.plot(x,y,'rx')
+
+
+def regresion(n,x,y):
+    dim = n + 1
+    A = createMatriz(dim,dim,0)
+    for i in range(dim):
+        for j in range(dim):
+            A[i][j] = sum(xi**(i+j) for xi in x)
+    C = createMatriz(dim,1,0)
+    for i in range(dim):
+        C[i][0] = sum(yi * xi**(i) for xi,yi in zip(x,y) )
+    invA = getMatrizInversa(A)
+    B = mulMatrices(invA,C)
+    return B
+
+coef = regresion(2,x,y)
+print("coeficientes",coef)
+
+
+x2 = np.linspace(0, 50, 100)
+
+def evalua(x,coef):
+    y = []
+    for i in range(len(x)):
+        y.append(0)
+        for j in range(len(coef)):
+            y[i] += coef[j][0] * x[i] **(j)
+    return y
+y2 = evalua(x2,coef)
+s=[12]
+y3 = evalua(s,coef)
+print("El rendimiento es: ")
+print(y3)
+plt.plot(x2,y2)
+plt.show()
